6e85467454c26598ee7a9773ad62195bbe450108
Chris Wu <WUCH8@OOCL.COM>
2021-06-29 20:37:53 +0800

    [previous] commit

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -1,38 +1,28 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class Parcel {
+    private User receiver;
+    private User sender;
     private String size;
     private Double weight;
-
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String receiverAddress;
-
-    private String senderName;
-    private String senderPhoneNumber;
-    private String senderAddress;
 
     public Parcel(String size, Double weight,
                   User receiver, User sender) {
         this.size = size;
         this.weight = weight;
-        this.receiverName = receiver.getReceiverName();
-        this.receiverPhoneNumber = receiver.getReceiverPhoneNumber();
-        this.receiverAddress = receiver.getReceiverAddress();
-        this.senderName = sender.getReceiverName();
-        this.senderPhoneNumber = sender.getReceiverPhoneNumber();
-        this.senderAddress = sender.getReceiverAddress();
+        this.receiver = receiver;
+        this.sender = sender;
     }
 
-    public String confirmReceiver(){
+    public String confirmReceiver() {
         return String.format("Please confirm receiver information: receiver name is %s, " +
                         "receiver phone number is %s and receiver address is %s.\n",
-                receiverName, receiverPhoneNumber, receiverAddress);
+                this.receiver.getName(), this.receiver.getPhoneNumber(), this.receiver.getAddress());
     }
 
-    public String confirmSender(){
+    public String confirmSender() {
         return String.format("Please confirm sender information: sender name is %s, " +
                         "sender phone number is %s and sender address is %s.\n",
-                senderName, senderPhoneNumber, senderAddress);
+                this.sender.getName(), this.sender.getPhoneNumber(), this.sender.getAddress());
     }
 }
--- src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -1,25 +1,25 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class User {
-    private final String receiverName;
-    private final String receiverPhoneNumber;
-    private final String receiverAddress;
+    private final String name;
+    private final String phoneNumber;
+    private final String address;
 
-    public User(String receiverName, String receiverPhoneNumber, String receiverAddress) {
-        this.receiverName = receiverName;
-        this.receiverPhoneNumber = receiverPhoneNumber;
-        this.receiverAddress = receiverAddress;
+    public User(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
 
-    public String getReceiverName() {
-        return receiverName;
+    public String getName() {
+        return name;
     }
 
-    public String getReceiverPhoneNumber() {
-        return receiverPhoneNumber;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public String getReceiverAddress() {
-        return receiverAddress;
+    public String getAddress() {
+        return address;
     }
 }
